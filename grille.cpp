4f545c8719f789d728e0f3e83b0de16277992e8b
Emna <aimuna.ayadi97@gmail.com>
2019-03-19 11:35:11 +0100

    first push

---
+++ grille.cpp
@@ -0,0 +1,28 @@
+#define COL 3
+#define LINE 4
+
+
+class plateau{
+
+	int player;
+	std::byte grid[LINE][COL] = {0};
+
+
+	void  play(int y,int x){
+		this->player;
+		
+
+
+
+	}
+
+	int get(){
+
+	}
+
+
+
+
+
+
+};
