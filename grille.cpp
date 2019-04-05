b1e5facaa3640398711eb84ffdbf92df974b0bf0
Emna <aimuna.ayadi97@gmail.com>
2019-04-05 15:43:26 +0200

    minimax

--- grille.cpp
+++ grille.cpp
@@ -156,11 +156,13 @@ void plateau:: upload(std::byte grid[LINE][COL]){
 	memcpy(this->grid,grid,sizeof(std::byte[LINE][COL]));
 }
 
-void plateau::move_dispo(std::byte grid[LINE][COL]){
-	play.clear();
+std::vector<int> plateau::move_dispo(std::byte grid[LINE][COL]){
+	std::vector<int> play;
 	for (int i = 0; i < COL; i++){
 		if (grid[LINE - 1][i] == std::byte{0}){
 			play.push_back(i);
+	
 		}
 	}
+	return play;
 }
--- minimax.cpp
+++ minimax.cpp
@@ -1,10 +1,14 @@
-#include"grille.hpp"
-#include"node.hpp"
+
 
 int best_move(plateau grid, int depth, std::byte player){
 
 	int neutral_valu = std::numeric_limits<int>::min(); //0x80000000
 	std::vector<int> choix;
+
+	for(auto m : grid.move_dispo()){
+		plateau tmp = grid;
+		tmp.play(m)
+	}
 
 
 
