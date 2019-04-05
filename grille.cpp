cfcfebc0b25b939487aafd0e50ef029223daf4f2
Emna <aimuna.ayadi97@gmail.com>
2019-04-05 15:35:16 +0200

    minimax

--- grille.cpp
+++ grille.cpp
@@ -29,129 +29,138 @@ std::byte plateau::getPlayer(){
 	return player;
 }
 
-	void plateau::display(){
-		for (int j = LINE - 1; j >= 0; j--){
-			for (int i = 0; i < COL; i++){
-				if (grid[j][i] == std::byte{254})
-					std::cout << "x";
-				else if (grid[j][i] == std::byte{1})
-					std::cout << "O";
-				else
-					std::cout << ".";
-			}
-			std::cout  << "\n";
-		}
-	}
-
-	int plateau::end(){
+void plateau::display(){
+	for (int j = LINE - 1; j >= 0; j--){
 		for (int i = 0; i < COL; i++){
-			if (grid[LINE - 1][i] == std::byte{0})
-				return 0;
+			if (grid[j][i] == std::byte{254})
+				std::cout << "x";
+			else if (grid[j][i] == std::byte{1})
+				std::cout << "O";
+			else
+				std::cout << ".";
 		}
-		return 1;
+		std::cout  << "\n";
 	}
+}
 
-	int plateau::verification_horizontal(){
-		int y = last_y;
-		int x = last_x;
-		int acc = 0;
-		int i = x;
-
-		while(i < COL && grid[y][i] == ~player)
-			i++;
-		i--;
-		while(i >= 0 && grid[y][i] == ~player){
-			acc ++;
-			i--;
-		}
-		return acc;
-
+int plateau::end(){
+	for (int i = 0; i < COL; i++){
+		if (grid[LINE - 1][i] == std::byte{0})
+			return 0;
 	}
+	return 1;
+}
 
-	int plateau::verification_vertical(){
-		int y = last_y;
-		int x = last_x;
-		int acc = 0;
-		int j = y;
+int plateau::verification_horizontal(){
+	int y = last_y;
+	int x = last_x;
+	int acc = 0;
+	int i = x;
 
-		while(j < LINE && grid[j][x] == ~player)
-			j++;
-		j--;
-		while(j >= 0 && grid[j][x] == ~player){
-			acc ++;
-			j--;
-		}
-		return acc;
-	}
-
-	int plateau::verification_diagonal_gd(){
-		int y = last_y;
-		int x = last_x;
-		int acc = 0;
-		int i = x;
-		int j = y;
-		while(j < LINE && i < COL && grid[j][i] == ~player){
-			j++;
-			i++;
-		}
-		i--;
-		j--;
-		while(j >= 0 && i >= 0 && grid[j][i] == ~player){
-			acc++;
-			j--;
-			i--;
-		}
-		return acc;
-	}
-
-	int plateau::verification_diagonal_dg(){
-		int y = last_y;
-		int x = last_x;
-		int i = x;
-		int j = y;
-		int acc =  0;
-		while(j < LINE && i >= 0 && grid[j][i] == ~player){
-			j++;
-			i--;
-		}
+	while(i < COL && grid[y][i] == ~player)
 		i++;
+	i--;
+	while(i >= 0 && grid[y][i] == ~player){
+		acc ++;
+		i--;
+	}
+	return acc;
+
+}
+
+int plateau::verification_vertical(){
+	int y = last_y;
+	int x = last_x;
+	int acc = 0;
+	int j = y;
+
+	while(j < LINE && grid[j][x] == ~player)
+		j++;
+	j--;
+	while(j >= 0 && grid[j][x] == ~player){
+		acc ++;
 		j--;
-		while(j >= 0 && i < COL && grid[j][i] == ~player){
-			acc++;
-			j--;
-			i++;
-		}
-		return acc;
 	}
+	return acc;
+}
 
-  std::byte plateau::who_win(int acc){
-		if(acc == WIN){
-			if(grid[last_y][last_x] == std::byte{1})
-				return std::byte{1};
-			if(grid[last_y][last_x] == std::byte{254})
-				return std::byte{254};
-		}
-		return std::byte{0};
+int plateau::verification_diagonal_gd(){
+	int y = last_y;
+	int x = last_x;
+	int acc = 0;
+	int i = x;
+	int j = y;
+	while(j < LINE && i < COL && grid[j][i] == ~player){
+		j++;
+		i++;
 	}
-
-  std::byte plateau::verification(){
-    std::byte horizon = who_win(verification_horizontal());
-    std::byte vertical = who_win(verification_vertical());
-    std::byte diag_gd = who_win(verification_diagonal_gd());
-    std::byte diag_dg = who_win(verification_diagonal_dg());
-
-		if(horizon != std::byte{0})
-			return horizon;
-		if(vertical != std::byte{0})
-			return vertical;
-		if(diag_gd != std::byte{0})
-			return diag_gd;
-		if(diag_dg != std::byte{0})
-			return diag_dg;
-		return std::byte{0};
+	i--;
+	j--;
+	while(j >= 0 && i >= 0 && grid[j][i] == ~player){
+		acc++;
+		j--;
+		i--;
 	}
-	void plateau:: upload(std::byte grid[LINE][COL]){
+	return acc;
+}
+
+int plateau::verification_diagonal_dg(){
+	int y = last_y;
+	int x = last_x;
+	int i = x;
+	int j = y;
+	int acc =  0;
+	while(j < LINE && i >= 0 && grid[j][i] == ~player){
+		j++;
+		i--;
+	}
+	i++;
+	j--;
+	while(j >= 0 && i < COL && grid[j][i] == ~player){
+		acc++;
+		j--;
+		i++;
+	}
+	return acc;
+}
+
+std::byte plateau::who_win(int acc){
+	if(acc == WIN){
+		if(grid[last_y][last_x] == std::byte{1})
+			return std::byte{1};
+		if(grid[last_y][last_x] == std::byte{254})
+			return std::byte{254};
+	}
+	return std::byte{0};
+}
+
+std::byte plateau::verification(){
+	std::byte horizon = who_win(verification_horizontal());
+	std::byte vertical = who_win(verification_vertical());
+	std::byte diag_gd = who_win(verification_diagonal_gd());
+	std::byte diag_dg = who_win(verification_diagonal_dg());
+
+	if(horizon != std::byte{0})
+		return horizon;
+	if(vertical != std::byte{0})
+		return vertical;
+	if(diag_gd != std::byte{0})
+		return diag_gd;
+	if(diag_dg != std::byte{0})
+		return diag_dg;
+	return std::byte{0};
+}
+void plateau:: upload(std::byte grid[LINE][COL]){
 		//std::byte copie[LINE][COL];
 
-		memcpy(this->grid,grid,sizeof(std::byte[LINE][COL]));
+	memcpy(this->grid,grid,sizeof(std::byte[LINE][COL]));
+}
+
+void plateau::move_dispo(std::byte grid[LINE][COL]){
+	play.clear();
+	for (int i = 0; i < COL; i++){
+		if (grid[LINE - 1][i] == std::byte{0}){
+			play.push_back(i);
+		}
 	}
+}
---
+++ minimax.cpp
@@ -0,0 +1,14 @@
+#include"grille.hpp"
+#include"node.hpp"
+
+int best_move(plateau grid, int depth, std::byte player){
+
+	int neutral_valu = std::numeric_limits<int>::min(); //0x80000000
+	std::vector<int> choix;
+
+
+
+
+}
+
+int Minimax(node_t node, int depth, std::byte player)
