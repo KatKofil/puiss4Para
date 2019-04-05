2add0280d37fe83aca400713ef30676a0baae447
Kofil <kat.kofil@yahoo.fr>
2019-04-05 16:33:44 +0200

    Avec le header c'est mieux

---
+++ minimax.hpp
@@ -0,0 +1,14 @@
+#ifndef _MINIMAX_HPP_
+#define _MINIMAX_HPP
+
+#include "utils.hpp"
+#include "grille.hpp"
+#include "node.hpp"
+
+class Minimax{
+  public:
+    int best_move(plateau grid, int deth);
+    int minimax(Node node);
+};
+
+#endif
