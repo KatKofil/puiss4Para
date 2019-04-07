f7a51aa9896285e45bea81316d524e7e87f48754
Emna <aimuna.ayadi97@gmail.com>
2019-04-07 20:13:17 +0200

    copie de letat de le grille dans tmp

--- node.cpp
+++ node.cpp
@@ -51,9 +51,9 @@ void Node::create_succes(int _nbSucces){
   
   this->nbSucces = _nbSucces;
   for (int i = 0; i < _nbSucces; i++){
-    std::cout << i << "\n";
-  	Node *tmp;
-    tmp->etat = this->etat;
+    //std::cout << i << "\n";
+  	Node *tmp = new Node;
+    tmp->etat.upload(etat.grid);
     tmp->parent = this;
     tmp->cal_level();
     succes.push_back(std::move(tmp));
